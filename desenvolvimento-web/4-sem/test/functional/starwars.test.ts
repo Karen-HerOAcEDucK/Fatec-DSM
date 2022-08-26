f362193e475bd70f5a6226af1654aba451a33361
Karen Letícia Alexandre <heroace44@gmail.com>
2022-08-25 21:53:12 -0300

    Finalizados entendimento de testes em typescript

--- desenvolvimento-web/4-sem/test/functional/starwars.test.ts
+++ desenvolvimento-web/4-sem/test/functional/starwars.test.ts
@@ -2,31 +2,32 @@
 
 describe('Valida o comportamento de uma função', () => {
 
-    function soma(a:number, b:number): number{
-        return a + b
+    class Operation {
+        soma(a:number, b:number): number{
+            return a + b
+        }
+    
+        sub(a:number, b:number): number{
+            return a - b
+        }
+    
+        multi(a:number, b:number): number{
+            return a * b
+        }
     }
-
-    function sub(a:number, b:number): number{
-        return a - b
-    }
-
-    function multi(a:number, b:number): number{
-        return a * b
-    }
-
 
     it('Soma de a + b', () => {
-        const result: number = soma(10, 10)
+        const result: number = new Operation().soma(10, 10)
         expect(result).toBe(20)
     })
     
     it('Soma de a - b', () => {
-        const result: number = sub(10, 10)
+        const result: number = new Operation().sub(10, 10)
         expect(result).toBe(0)
     })
     
     it('Multi de a * b', () => {
-        const result: number = multi(10, 10)
+        const result: number = new Operation().multi(10, 10)
         expect(result).toBe(100)
     })
 })
