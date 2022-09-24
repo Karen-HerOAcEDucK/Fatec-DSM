8d355cc6d2673093a439ceaf7bc9817c6019f1c8
Karen Letícia Alexandre <heroace44@gmail.com>
2022-09-23 22:07:09 -0300

    Adicionando codigos dos aplicativos produzidos em aula

---
+++ desenvolvimento-mobile/sorteiodado/app/src/main/java/com/example/sorteio_dado/resultSorteio.kt
@@ -0,0 +1,11 @@
+package com.example.sorteio_dado
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class resultSorteio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result_sorteio)
+    }
+}
